a58863a242a5819ff00614ae1607223e565e18be
Ayush Jain <ayujain2004@gmail.com>
2023-10-05 00:19:34 -0400

    frontend v1 backend final v :

--- db.js
+++ db.js
@@ -1,132 +1,96 @@
-
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  password: "postgres",
-  host: "localhost",
-  port: 5432,
-  database: "ratemytoilet",
+    user: "postgres",
+    password: "postgres",
+    host: "localhost",
+    port: 5432,
+    database: "ratemytoilet",
 });
 
-
-const addReview = (review) => {
-  return new Promise((resolve, reject) => {
-    const query = 'INSERT INTO reviews(username, bathroom_id, cleanliness, poopability, overall_rating, peacefulness, additional_comments) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *';
-    const params = [review.username, review.bathroom_id, review.cleanliness, review.poopability, review.overall_rating, review.peacefulness, review.additional_comments];
-
-    pool.query(query, params, (error, results) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(results.rows);
-      }
-    });
-  });
+const addReview = async (review) => {
+    const query = `INSERT INTO reviews (username, bathroom_id, cleanliness, poopability, overall_rating, peacefulness, additional_comments)
+                   VALUES ($1, $2, $3, $4, $5, $6, $7)`;
+    const values = [review.username, review.bathroom_id, review.cleanliness, review.poopability, review.overall_rating, review.peacefulness, review.additional_comments];
+    await pool.query(query, values);
 };
 
-const viewReviews = (buildingName) => {
-  return new Promise((resolve, reject) => {
+const viewReviews = async (buildingName) => {
     const query = `
-      SELECT r.*, b.building_name, b.floor_number, b.gender
-      FROM reviews r
-      JOIN bathroom b ON r.bathroom_id = b.bathroom_id
-      WHERE b.building_name = $1
+        SELECT r.*, b.building_name, b.floor_number, b.gender
+        FROM reviews r
+        JOIN bathroom b ON r.bathroom_id = b.bathroom_id
+        WHERE b.building_name = $1
     `;
-    const params = [buildingName];
-
-    pool.query(query, params, (error, results) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(results.rows);
-      }
-    });
-  });
+    const { rows } = await pool.query(query, [buildingName]);
+    return rows;
 };
 
-
-
-const highestReviewByOverallRating = (buildingName) => {
-  return new Promise((resolve, reject) => {
-    const query = 'SELECT * FROM reviews WHERE building_name = $1 ORDER BY overall_rating DESC LIMIT 1';
-    const params = [buildingName];
-
-    pool.query(query, params, (error, results) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(results.rows[0]);
-      }
-    });
-  });
+const highestReviewByOverallRating = async (buildingName) => {
+    const query = `
+        SELECT r.*, b.building_name, b.floor_number, b.gender
+        FROM reviews r
+        JOIN bathroom b ON r.bathroom_id = b.bathroom_id
+        WHERE b.building_name = $1
+        ORDER BY r.overall_rating DESC
+        LIMIT 1
+    `;
+    const { rows } = await pool.query(query, [buildingName]);
+    return rows[0];
 };
 
-const highestReviewByPoopability = (buildingName) => {
-  return new Promise((resolve, reject) => {
-    const query = 'SELECT * FROM reviews WHERE building_name = $1 ORDER BY poopability DESC LIMIT 1';
-    const params = [buildingName];
-
-    pool.query(query, params, (error, results) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(results.rows[0]);
-      }
-    });
-  });
+const highestReviewByPoopability = async (buildingName) => {
+    const query = `
+        SELECT r.*, b.building_name, b.floor_number, b.gender
+        FROM reviews r
+        JOIN bathroom b ON r.bathroom_id = b.bathroom_id
+        WHERE b.building_name = $1
+        ORDER BY r.poopability DESC
+        LIMIT 1
+    `;
+    const { rows } = await pool.query(query, [buildingName]);
+    return rows[0];
 };
 
-const highestReviewByCryability = (buildingName) => {
-  return new Promise((resolve, reject) => {
-    const query = 'SELECT * FROM reviews WHERE building_name = $1 ORDER BY peacefulness DESC LIMIT 1';
-    const params = [buildingName];
-
-    pool.query(query, params, (error, results) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(results.rows[0]);
-      }
-    });
-  });
+const highestReviewByPeacefulness = async (buildingName) => {
+    const query = `
+        SELECT r.*, b.building_name, b.floor_number, b.gender
+        FROM reviews r
+        JOIN bathroom b ON r.bathroom_id = b.bathroom_id
+        WHERE b.building_name = $1
+        ORDER BY r.peacefulness DESC
+        LIMIT 1
+    `;
+    const { rows } = await pool.query(query, [buildingName]);
+    return rows[0];
 };
 
-const viewBathroomsByFilter = (gender, floorNumber, buildingName) => {
-  return new Promise((resolve, reject) => {
-    const query = 'SELECT * FROM bathroom WHERE gender = $1 AND floor_number = $2 AND building_name = $3';
-    const params = [gender, floorNumber, buildingName];
-
-    pool.query(query, params, (error, results) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(results.rows);
-      }
-    });
-  });
+const viewBathroomsByFilter = async (gender, floorNumber, buildingName) => {
+    const query = `
+        SELECT * FROM bathroom
+        WHERE gender = $1 AND floor_number = $2 AND building_name = $3
+    `;
+    const { rows } = await pool.query(query, [gender, floorNumber, buildingName]);
+    return rows;
 };
 
-const topReviewers = () => {
-  return new Promise((resolve, reject) => {
-    const query = 'SELECT username, COUNT(*) as review_count FROM reviews GROUP BY username ORDER BY review_count DESC LIMIT 1';
-    
-    pool.query(query, [], (error, results) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(results.rows);
-      }
-    });
-  });
+const topReviewers = async () => {
+    const query = `
+        SELECT username, COUNT(review_id) as review_count
+        FROM reviews
+        GROUP BY username
+        ORDER BY review_count DESC
+    `;
+    const { rows } = await pool.query(query);
+    return rows;
 };
 
 module.exports = {
-  addReview,
-  viewReviews,
-  highestReviewByOverallRating,
-  highestReviewByPoopability,
-  highestReviewByCryability,
-  viewBathroomsByFilter,
-  topReviewers
+    addReview,
+    viewReviews,
+    highestReviewByOverallRating,
+    highestReviewByPoopability,
+    highestReviewByPeacefulness,
+    viewBathroomsByFilter,
+    topReviewers
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,75 +1,82 @@
 
 const express = require('express');
 const router = express.Router();
-const db = require('../db');  // Assume a db.js file for database operations
+const db = require('../db');
 
 // 1) Add Reviews
-router.post('/addReview', (req, res) => {
-    const review = req.body;
-    db.addReview(review).then(() => {
+router.post('/addReview', async (req, res) => {
+    try {
+        const review = req.body;
+        await db.addReview(review);
         res.status(200).send("Review added successfully");
-    }).catch(err => {
+    } catch (err) {
         res.status(500).send(err.message);
-    });
+    }
 });
 
 // 2) View Reviews for a building_name
-router.get('/viewReviews/:buildingName', (req, res) => {
-    const buildingName = req.params.buildingName;
-    db.viewReviews(buildingName).then(reviews => {
+router.get('/viewReviews/:buildingName', async (req, res) => {
+    try {
+        const buildingName = req.params.buildingName;
+        const reviews = await db.viewReviews(buildingName);
         res.status(200).json(reviews);
-    }).catch(err => {
+    } catch (err) {
         res.status(500).send(err.message);
-    });
+    }
 });
 
 // 3) View highest review in building by overall_rating
-router.get('/highestReview/:buildingName', (req, res) => {
-    const buildingName = req.params.buildingName;
-    db.highestReviewByOverallRating(buildingName).then(review => {
+router.get('/highestReview/overall/:buildingName', async (req, res) => {
+    try {
+        const buildingName = req.params.buildingName;
+        const review = await db.highestReviewByOverallRating(buildingName);
         res.status(200).json(review);
-    }).catch(err => {
+    } catch (err) {
         res.status(500).send(err.message);
-    });
+    }
 });
 
 // 4) View highest review in building by poopability
-router.get('/highestPoopabilityReview/:buildingName', (req, res) => {
-    const buildingName = req.params.buildingName;
-    db.highestReviewByPoopability(buildingName).then(review => {
+router.get('/highestReview/poopability/:buildingName', async (req, res) => {
+    try {
+        const buildingName = req.params.buildingName;
+        const review = await db.highestReviewByPoopability(buildingName);
         res.status(200).json(review);
-    }).catch(err => {
+    } catch (err) {
         res.status(500).send(err.message);
-    });
+    }
 });
 
 // 5) View highest review in building by cryability (assuming this is peacefulness)
-router.get('/highestCryabilityReview/:buildingName', (req, res) => {
-    const buildingName = req.params.buildingName;
-    db.highestReviewByCryability(buildingName).then(review => {
+router.get('/highestReview/cryability/:buildingName', async (req, res) => {
+    try {
+        const buildingName = req.params.buildingName;
+        const review = await db.highestReviewByCryability(buildingName);
         res.status(200).json(review);
-    }).catch(err => {
+    } catch (err) {
         res.status(500).send(err.message);
-    });
+    }
 });
 
 // 6) View bathrooms by gender, floor_number, and building_name
-router.get('/viewBathrooms', (req, res) => {
-    const { gender, floorNumber, buildingName } = req.query;
-    db.viewBathroomsByFilter(gender, floorNumber, buildingName).then(bathrooms => {
+router.get('/viewBathrooms', async (req, res) => {
+    try {
+        const { gender, floorNumber, buildingName } = req.query;
+        const bathrooms = await db.viewBathroomsByFilter(gender, floorNumber, buildingName);
         res.status(200).json(bathrooms);
-    }).catch(err => {
+    } catch (err) {
         res.status(500).send(err.message);
-    });
+    }
 });
 
 // 7) View users who have the highest number of ratings written
-router.get('/topReviewers', (req, res) => {
-    db.topReviewers().then(users => {
+router.get('/topReviewers', async (req, res) => {
+    try {
+        const users = await db.topReviewers();
         res.status(200).json(users);
-    }).catch(err => {
+    } catch (err) {
         res.status(500).send(err.message);
-    });
+    }
 });
 
 module.exports = router;
